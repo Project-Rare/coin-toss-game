64015821614517d8e67bf2d32468410aa816f4ba
Yoalsfbin <y.yokoyfb@gmail.com>
2025-06-17 21:05:42 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CoinTossController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('game');
 });
+
+Route::post('api/coin-toss', [CoinTossController::class, 'play']);
